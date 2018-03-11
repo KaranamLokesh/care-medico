640c016fe49b73db2303980edf090653c127f81b
Shubham Karmakar <shubdev07@gmail.com>
2018-03-11 11:13:01 +0530

    Added the update NGO info code

--- server/api/NGO/ngo.controller.js
+++ server/api/NGO/ngo.controller.js
@@ -1,5 +1,34 @@
 const ngoModel = require("./ngo.model");
 
 module.exports.signup = (req, res) => {
-  //business logic here
+    //business logic here
+};
+
+
+module.exports.updateInfo = (req, res) => {
+    ngoModel.findOneAndUpdate({
+            "_id": req.body.id
+        }, {
+            "$set": req.body
+        }, {
+            new: true
+        },
+        (err, doc) => {
+            if (err) {
+                res.send(err);
+            } else if (!doc) {
+
+                res.status(404).json({
+                    success: false,
+                    message: "No such NGO found"
+                });
+            } else {
+                res.status(200).json({
+                    success: true,
+                    message: "NGO info has been updated successfully",
+                    doc: doc
+                });
+            }
+        }
+    );
 };
--- server/api/index.js
+++ server/api/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const ngoController = require("./NGO/ngo.controller");
 
 router.post("/ngo/new", ngoController.signup);
+router.put("/ngo/edit", ngoController.updateInfo);
 
 module.exports = router;
