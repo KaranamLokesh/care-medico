e934a28ec405adc20a9b0086128055ce770ee5d1
Lokesh Karanam <sunnylokesh3@gmail.com>
2018-03-14 01:29:15 +0530

    Add files via upload

---
+++ app/routes.js
@@ -0,0 +1,57 @@
+var User = require('./models/user');
+module.exports = function(app, passport){
+	app.get('/', function(req, res){
+		res.render('index.ejs');
+	});
+
+	app.get('/login', function(req, res){
+		res.render('login.ejs', { message: req.flash('loginMessage') });
+	});
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect: '/profile',
+		failureRedirect: '/login',
+		failureFlash: true
+	}));
+
+	app.get('/signup', function(req, res){
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
+	});
+
+
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect: '/',
+		failureRedirect: '/signup',
+		failureFlash: true
+	}));
+
+	app.get('/profile', isLoggedIn, function(req, res){
+		res.render('profile.ejs', { user: req.user });
+	});
+
+	// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/profile',
+                                      failureRedirect: '/login' }));        
+
+
+	app.get('/logout', function(req, res){
+		req.logout();
+		res.redirect('/');
+	})
+};
+
+function isLoggedIn(req, res, next) {
+	if(req.isAuthenticated()){
+		return next();
+	}
+
+	res.redirect('/login');
+}
---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '906097719555790',
+		'clientSecret': '759671deb9dfecd67fcc087a5079622d',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+	}
+
+	
+}
